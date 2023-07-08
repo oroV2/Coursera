fcdddc4b1ac0e0c577f2621da6be3d5a186f8392
ONKARPAWAR7598 <onkar@expenzing.com>
2023-07-08 11:17:27 +0530

    Geusture part 2

--- ProjectForTest/components/DishDetailComponent.js
+++ ProjectForTest/components/DishDetailComponent.js
@@ -52,7 +52,10 @@ function RenderComments(props) {
 }
 
 function RenderDish(props) {
+
     const dish = props.dish;
+
+    handleViewRef = ref => this.view = ref;
 
     const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
         if (dx < -200)
@@ -65,6 +68,7 @@ function RenderDish(props) {
         onStartShouldSetPanResponder: (e, gestureState) => {
             return true;
         },
+        onPanResponderGrant: () => { this.view.rubberBand(1000).then(endState => console.log(endState.finished ? 'finished' : 'cancelled')); },
         onPanResponderEnd: (e, gestureState) => {
             console.log("pan responder end", gestureState);
             if (recognizeDrag(gestureState))
@@ -84,7 +88,9 @@ function RenderDish(props) {
 
     if (dish != null) {
         return (
-            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}>
+            <Animatable.View animation="fadeInDown" duration={2000} delay={1000}
+                ref={this.handleViewRef}
+                {...panResponder.panHandlers}>
                 <Card
                     featuredTitle={dish.name}
                     image={{ uri: baseUrl + dish.image }}>
