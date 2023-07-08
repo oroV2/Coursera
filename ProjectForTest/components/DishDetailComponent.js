7044fe7d605ceca9939d95fee20e7822bb99e62c
ONKARPAWAR7598 <onkar@expenzing.com>
2023-07-08 11:12:14 +0530

    Geusture part 1

--- ProjectForTest/components/DishDetailComponent.js
+++ ProjectForTest/components/DishDetailComponent.js
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
-import { Text, View, ScrollView, FlatList } from 'react-native';
+import { Text, View, ScrollView, FlatList, Modal, StyleSheet, Button, Alert, PanResponder } from 'react-native';
 import { Card, Icon } from '@rneui/themed';
 import { DISHES } from '../shared/dishesh';
 import { COMMENTS } from '../shared/comments';
@@ -53,6 +53,34 @@ function RenderComments(props) {
 
 function RenderDish(props) {
     const dish = props.dish;
+
+    const recognizeDrag = ({ moveX, moveY, dx, dy }) => {
+        if (dx < -200)
+            return true;
+        else
+            return false;
+    }
+
+    const panResponder = PanResponder.create({
+        onStartShouldSetPanResponder: (e, gestureState) => {
+            return true;
+        },
+        onPanResponderEnd: (e, gestureState) => {
+            console.log("pan responder end", gestureState);
+            if (recognizeDrag(gestureState))
+                Alert.alert(
+                    'Add Favorite',
+                    'Are you sure you wish to add ' + dish.name + ' to favorite?',
+                    [
+                        { text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel' },
+                        { text: 'OK', onPress: () => { props.favorite ? console.log('Already favorite') : props.onPress() } },
+                    ],
+                    { cancelable: false }
+                );
+
+            return true;
+        }
+    })
 
     if (dish != null) {
         return (
